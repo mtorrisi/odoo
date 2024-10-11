ac50e2facfd858693f6df8d1a9c6d0997aa6ae3f
mtorrisi <torrisi.mario@gmail.com>
2024-10-11 11:06:35 +0200

    Chapter 5

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -1,6 +1,10 @@
 {
     "name": "Real estate",
     'summary': 'Sample Tutorial',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml'
+    ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
--- tutorials/estate/models/estate_property.py
+++ tutorials/estate/models/estate_property.py
@@ -1,8 +1,29 @@
 from odoo import fields, models
 
+
 class EstatePropery(models.Model):
     _name = "estate_property"
     _description = "Property Model"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
+    posstcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string="Type",
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        help="Type is used to separate Leads and Opportunities",
+    )
