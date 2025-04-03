51c0c83f01396f36cfa1228790d7f9d6be3026d3
mtorrisi <torrisi.mario@gmail.com>
2025-04-03 15:48:06 +0200

    Chapter #4

--- tutorials/real_estate/__manifest__.py
+++ tutorials/real_estate/__manifest__.py
@@ -22,7 +22,7 @@ The best module ever made.
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         # 'views/views.xml',
         # 'views/templates.xml',
     ],
